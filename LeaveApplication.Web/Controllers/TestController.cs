7d27d6768917bfa3c0f522f95fe819d79bf2da47
komal.mule <komal.mule@DTPL-ASSET-2024>
2026-04-23 18:25:47 +0530

    Add project files.

---
+++ LeaveApplication.Web/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using LeaveApplication.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveApplication.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                name = "Komal Ramesh Mule",
+                DateOfBirth = new DateTime(2005,02,24)
+            };
+
+
+            return View(data);
+        }
+    }
+}
---
+++ LeaveApplication.Web/Models/TestViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveApplication.Web.Models
+{
+    public class TestViewModel
+    {
+        public string name { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
